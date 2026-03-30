cb824acf731ed107f16909c800b359c0b4247a72
leandromartens <leandromartens2006@gmail.com>
2026-03-30 16:51:54 -0300

    Domain do Veículo

---
+++ src/main/java/br/edu/ifsul/sapucaia/projeto/domain/Veiculo.java
@@ -0,0 +1,45 @@
+package br.edu.ifsul.sapucaia.projeto.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idVeiculo;
+
+    private String modelo;
+
+    private LocalDate dataUltimaAtualizacaoKm;
+
+    private String marca;
+
+    private String placa;
+
+    private String tipo;
+
+    private int ano;
+
+    private String cor;
+
+    private double kmAtual;
+
+    @ManyToOne
+    @JoinColumn(name = "id_manutencao")
+    private Manutencao manutencao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_custo")
+    private Custo custo;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+    
