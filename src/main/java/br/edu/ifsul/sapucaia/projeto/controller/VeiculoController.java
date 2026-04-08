a4372c05635eba6efe33e8fd31e5fdaa168b1266
FelipeMartins152 <felipehomem152@gmail.com>
2026-04-08 20:10:02 -0300

    Controller Manutencao e Veiculo

---
+++ src/main/java/br/edu/ifsul/sapucaia/projeto/controller/VeiculoController.java
@@ -0,0 +1,12 @@
+package br.edu.ifsul.sapucaia.projeto.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/veiculo")
+@RequiredArgsConstructor
+public class VeiculoController {
+
+}
