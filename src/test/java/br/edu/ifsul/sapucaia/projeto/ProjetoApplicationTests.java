c23c68650d47fafe97e557213060ebfe1f03bb4a
NathanDoile <doile.souza@gmail.com>
2026-04-22 18:29:26 -0300

    Liberando testes unitários na pipeline e adicionando jacoco para cobertura de testes

--- src/test/java/br/edu/ifsul/sapucaia/projeto/ProjetoApplicationTests.java
+++ src/test/java/br/edu/ifsul/sapucaia/projeto/ProjetoApplicationTests.java
@@ -2,8 +2,10 @@ package br.edu.ifsul.sapucaia.projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjetoApplicationTests {
 
 	@Test
