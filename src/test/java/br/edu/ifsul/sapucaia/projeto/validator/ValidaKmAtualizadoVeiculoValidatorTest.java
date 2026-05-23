a90fe66a3e3c1f4d873b674d6ab390a9157b42d8
NathanDoile <doile.souza@gmail.com>
2026-05-23 16:37:12 -0300

    Resolvendo issues

--- src/test/java/br/edu/ifsul/sapucaia/projeto/validator/ValidaKmAtualizadoVeiculoValidatorTest.java
+++ src/test/java/br/edu/ifsul/sapucaia/projeto/validator/ValidaKmAtualizadoVeiculoValidatorTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ExtendWith(MockitoExtension.class)
-public class ValidaKmAtualizadoVeiculoValidatorTest {
+class ValidaKmAtualizadoVeiculoValidatorTest {
 
     @InjectMocks
     private ValidaKmAtualizadoVeiculoValidator tested;
