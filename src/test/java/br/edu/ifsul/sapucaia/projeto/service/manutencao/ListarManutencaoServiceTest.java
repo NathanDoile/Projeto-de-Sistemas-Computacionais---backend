480342cc7c8b09e958f1e607a13290eeee731698
leandromartens <leandromartens2006@gmail.com>
2026-06-01 21:11:39 -0300

    Implementação do test manutenção

---
+++ src/test/java/br/edu/ifsul/sapucaia/projeto/service/manutencao/ListarManutencaoServiceTest.java
@@ -0,0 +1,54 @@
+package br.edu.ifsul.sapucaia.projeto.service.manutencao;
+
+import br.edu.ifsul.sapucaia.projeto.domain.Manutencao;
+import br.edu.ifsul.sapucaia.projeto.repository.ManutencaoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ListarManutencaoServiceTest {
+
+    @InjectMocks
+    private ListarManutencaoService tested;
+
+    @Mock
+    private ManutencaoRepository manutencaoRepository;
+
+    @Test
+    @DisplayName("Deve listar as manutenções paginadas corretamente")
+    void deveListarManutencoesCorretamente() {
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Manutencao manutencao = Manutencao.builder()
+                .idManutencao(1L)
+                .descricao("Troca de óleo")
+                .isAtivo(true)
+                .build();
+
+        Page<Manutencao> page = new PageImpl<>(List.of(manutencao));
+
+        when(manutencaoRepository.findAllByIsAtivo(true, pageable))
+                .thenReturn(page);
+
+        Page<Manutencao> response = tested.listar(pageable);
+
+        verify(manutencaoRepository).findAllByIsAtivo(true, pageable);
+
+        assertEquals(1, response.getTotalElements());
+        assertEquals(manutencao.getIdManutencao(),
+                response.getContent().get(0).getIdManutencao());
+        assertEquals(manutencao.getDescricao(),
+                response.getContent().get(0).getDescricao());
+    }
+}
