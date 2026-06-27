2b69665b4307adac846c5e3d0e19d0c6999f46c3
FelipeMartins152 <felipehomem152@gmail.com>
2026-06-27 12:23:20 -0300

    Data da proxima manutenção pode ser null e ao atualizar o veiculo a IA é chamada caso IntervaloEntreManutencoesKm seja 0

--- src/test/java/br/edu/ifsul/sapucaia/projeto/service/veiculo/AtualizarKmVeiculoServiceTest.java
+++ src/test/java/br/edu/ifsul/sapucaia/projeto/service/veiculo/AtualizarKmVeiculoServiceTest.java
@@ -1,10 +1,13 @@
 package br.edu.ifsul.sapucaia.projeto.service.veiculo;
 
 import br.edu.ifsul.sapucaia.projeto.controller.request.veiculo.AtualizarKmVeiculoRequest;
+import br.edu.ifsul.sapucaia.projeto.controller.response.ia.ManutencaoIAResponse;
 import br.edu.ifsul.sapucaia.projeto.domain.Veiculo;
+import br.edu.ifsul.sapucaia.projeto.factory.IAFactory;
 import br.edu.ifsul.sapucaia.projeto.repository.VeiculoRepository;
 import br.edu.ifsul.sapucaia.projeto.security.UsuarioSecurity;
 import br.edu.ifsul.sapucaia.projeto.security.service.UsuarioAutenticadoService;
+import br.edu.ifsul.sapucaia.projeto.service.ia.IAService;
 import br.edu.ifsul.sapucaia.projeto.validator.ValidaKmAtualizadoVeiculoValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,8 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 import static br.edu.ifsul.sapucaia.projeto.factory.UsuarioFactory.usuarioSecurity;
 import static br.edu.ifsul.sapucaia.projeto.factory.VeiculoFactory.atualizarKmVeiculoRequest;
 import static br.edu.ifsul.sapucaia.projeto.factory.VeiculoFactory.veiculo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,16 +40,23 @@ class AtualizarKmVeiculoServiceTest {
     @Mock
     private ValidaKmAtualizadoVeiculoValidator validaKmAtualizadoVeiculoValidator;
 
+    @Mock
+    private IAService iaService;
+
     @Captor
     private ArgumentCaptor<Veiculo> veiculoCaptor;
 
     @Test
-    @DisplayName("Deve atualizar km do veículo corretamente")
-    void deveAtualizarKmVeiculoCorretamente() {
+    @DisplayName("Deve atualizar km do veículo corretamente - não chama é, informações de próximas manutenções diferentes de 0")
+    void deveAtualizarKmVeiculoCorretamenteNaoChamaIA() {
 
         AtualizarKmVeiculoRequest request = atualizarKmVeiculoRequest();
         UsuarioSecurity usuario = usuarioSecurity();
         Veiculo veiculo = veiculo();
+        veiculo.setIntervaloEntreManutencoesKm(5000);
+        veiculo.setIntervaloEntreManutencoesMeses(6);
+        veiculo.setProximaManutencaoKm(20000);
+        veiculo.setProximaManutencaoData(veiculo.getDataUltimaAtualizacaoKm().plusMonths(6));
 
         int kmAtualizado = (int) request.getKmAtualizado();
         int kmAtual = (int) veiculo.getKmAtual();
@@ -60,11 +69,82 @@ class AtualizarKmVeiculoServiceTest {
         verify(usuarioAutenticadoService).getUser();
         verify(veiculoRepository).findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true);
         verify(validaKmAtualizadoVeiculoValidator).maiorQueAtual(kmAtualizado, kmAtual);
+        verify(iaService, never()).chamadaChat(anyString());
         verify(veiculoRepository).save(veiculoCaptor.capture());
 
         Veiculo response = veiculoCaptor.getValue();
         assertEquals(request.getKmAtualizado(), response.getKmAtual());
+        assertEquals(response.getDataUltimaAtualizacaoKm().plusMonths(veiculo.getIntervaloEntreManutencoesMeses()), response.getProximaManutencaoData());
+        assertEquals(5000, response.getIntervaloEntreManutencoesKm());
+        assertEquals(6, response.getIntervaloEntreManutencoesMeses());
+    }
+
+    @Test
+    @DisplayName("Deve atualizar km do veículo corretamente - chama IA, pois km entre manutenções é 0")
+    void deveAtualizarKmVeiculoCorretamenteChamaIA() {
+
+        AtualizarKmVeiculoRequest request = atualizarKmVeiculoRequest();
+        UsuarioSecurity usuario = usuarioSecurity();
+        Veiculo veiculo = veiculo();
+
+        int kmAtualizado = (int) request.getKmAtualizado();
+        int kmAtual = (int) veiculo.getKmAtual();
+
+        ManutencaoIAResponse iaresponse = IAFactory.response();
+
+        when(usuarioAutenticadoService.getUser()).thenReturn(usuario);
+        when(veiculoRepository.findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true)).thenReturn(veiculo);
+        when(iaService.chamadaChat(anyString())).thenReturn(iaresponse);
+
+        tested.atualizar(request);
+
+        verify(usuarioAutenticadoService).getUser();
+        verify(veiculoRepository).findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true);
+        verify(validaKmAtualizadoVeiculoValidator).maiorQueAtual(kmAtualizado, kmAtual);
+        verify(iaService).chamadaChat(anyString());
+        verify(veiculoRepository).save(veiculoCaptor.capture());
+
+        Veiculo response = veiculoCaptor.getValue();
+        assertEquals(request.getKmAtualizado(), response.getKmAtual());
+        assertEquals(response.getIntervaloEntreManutencoesKm(), iaresponse.proximaRevisao().intervaloManutencoesKm());
+        assertEquals(response.getIntervaloEntreManutencoesMeses(), iaresponse.proximaRevisao().intervaloManutencoesMeses());
+        assertEquals(response.getProximaManutencaoKm(), request.getKmAtualizado() + iaresponse.proximaRevisao().distanciaRestanteKm());
+        assertEquals(response.getProximaManutencaoData(), response.getDataUltimaAtualizacaoKm().plusMonths(iaresponse.proximaRevisao().intervaloManutencoesMeses()));
         
+    }
+
+    @Test
+    @DisplayName("Deve atualizar km do veículo corretamente - chama IA, pois km entre manutenções é 0, mas erro na IA")
+    void deveAtualizarKmVeiculoCorretamenteChamaIAComErro() {
+
+        AtualizarKmVeiculoRequest request = atualizarKmVeiculoRequest();
+        UsuarioSecurity usuario = usuarioSecurity();
+        Veiculo veiculo = veiculo();
+
+        int kmAtualizado = (int) request.getKmAtualizado();
+        int kmAtual = (int) veiculo.getKmAtual();
+
+        ManutencaoIAResponse iaresponse = IAFactory.response();
+
+        when(usuarioAutenticadoService.getUser()).thenReturn(usuario);
+        when(veiculoRepository.findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true)).thenReturn(veiculo);
+        when(iaService.chamadaChat(anyString())).thenThrow(new RuntimeException("IA indisponível"));
+
+        tested.atualizar(request);
+
+        verify(usuarioAutenticadoService).getUser();
+        verify(veiculoRepository).findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true);
+        verify(validaKmAtualizadoVeiculoValidator).maiorQueAtual(kmAtualizado, kmAtual);
+        verify(iaService).chamadaChat(anyString());
+        verify(veiculoRepository).save(veiculoCaptor.capture());
+
+        Veiculo response = veiculoCaptor.getValue();
+        assertEquals(request.getKmAtualizado(), response.getKmAtual());
+        assertEquals(0, response.getIntervaloEntreManutencoesKm());
+        assertEquals(0, response.getIntervaloEntreManutencoesMeses());
+        assertEquals(0, response.getProximaManutencaoKm());
+        assertNull(response.getProximaManutencaoData());
+
     }
 
     @Test
@@ -89,6 +169,7 @@ class AtualizarKmVeiculoServiceTest {
         verify(usuarioAutenticadoService).getUser();
         verify(veiculoRepository).findByUsuarioIdUsuarioAndIsAtivo(usuario.getId(), true);
         verify(validaKmAtualizadoVeiculoValidator).maiorQueAtual(kmAtualizado, kmAtual);
+        verify(iaService, never()).chamadaChat(anyString());
         verify(veiculoRepository, never()).save(any(Veiculo.class));
 
     }
--- src/test/java/br/edu/ifsul/sapucaia/projeto/service/veiculo/CadastrarVeiculoServiceTest.java
+++ src/test/java/br/edu/ifsul/sapucaia/projeto/service/veiculo/CadastrarVeiculoServiceTest.java
@@ -13,7 +13,6 @@ import br.edu.ifsul.sapucaia.projeto.service.ia.IAService;
 import br.edu.ifsul.sapucaia.projeto.service.validator.ValidaUsuarioComVeiculoService;
 import br.edu.ifsul.sapucaia.projeto.service.validator.ValidaUsuarioService;
 import br.edu.ifsul.sapucaia.projeto.service.validator.ValidaVeiculoService;
-import br.edu.ifsul.sapucaia.projeto.validator.ValidaAnoVeiculoValidator;
 import br.edu.ifsul.sapucaia.projeto.validator.ValidaTipoVeiculoValidator;
 import br.edu.ifsul.sapucaia.projeto.validator.ValidarPlacaValidator;
 import org.junit.jupiter.api.DisplayName;
@@ -28,8 +27,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static br.edu.ifsul.sapucaia.projeto.factory.UsuarioFactory.usuario;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -152,7 +150,7 @@ class CadastrarVeiculoServiceTest {
         assertEquals(0, veiculoResponse.getIntervaloEntreManutencoesKm());
         assertEquals(0, veiculoResponse.getIntervaloEntreManutencoesMeses());
         assertEquals(0, veiculoResponse.getProximaManutencaoKm());
-        assertEquals(veiculoResponse.getProximaManutencaoData(), veiculoResponse.getDataUltimaAtualizacaoKm().plusMonths(0));
+        assertNull(veiculoResponse.getProximaManutencaoData());
         assertEquals(veiculoResponse, usuarioResponse.getVeiculo());
         assertTrue(usuarioResponse.isPossuiVeiculo());
     }
