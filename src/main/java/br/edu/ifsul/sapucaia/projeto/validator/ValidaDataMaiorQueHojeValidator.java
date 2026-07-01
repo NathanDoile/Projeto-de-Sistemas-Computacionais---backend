6a6bfea5ae47ae466252ade7427708413d233299
FelipeMartins152 <felipehomem152@gmail.com>
2026-07-01 20:30:26 -0300

    Correções

--- src/main/java/br/edu/ifsul/sapucaia/projeto/validator/ValidaDataMaiorQueHojeValidator.java
+++ src/main/java/br/edu/ifsul/sapucaia/projeto/validator/ValidaDataMaiorQueHojeValidator.java
@@ -1,12 +1,14 @@
 package br.edu.ifsul.sapucaia.projeto.validator;
 
 import br.edu.ifsul.sapucaia.projeto.helper.DateNow;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+@Component
 public class ValidaDataMaiorQueHojeValidator {
 
     public void naoMaiorQueHoje(LocalDate data){
