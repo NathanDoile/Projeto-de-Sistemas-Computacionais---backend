d32c09d431181b00c69261378fdabc8f2cd064fa
NathanDoile <doile.souza@gmail.com>
2026-07-01 18:24:14 -0300

    Padronizando mensagens de erros nos request

--- src/main/java/br/edu/ifsul/sapucaia/projeto/controller/request/ErroRequest.java
+++ src/main/java/br/edu/ifsul/sapucaia/projeto/controller/request/ErroRequest.java
@@ -13,4 +13,8 @@ public class ErroRequest {
     public static final String CAMPO_PLACA = "deve ter 7 caracteres, sem hífen.";
 
     public static final int CAMPO_TAMANHO_MAXIMO_MINIMO_PLACA = 7;
+
+    public static final String CAMPO_CODIGO_RECUPERAR_SENHA = "deve conter 8 caracteres.";
+
+    public static final int CAMPO_TAMANHO_MAXIMO_MINIMO_CODIGO_RCUPERAR_SENHA = 8;
 }
--- src/main/java/br/edu/ifsul/sapucaia/projeto/security/controller/request/EnviarEmailRequest.java
+++ src/main/java/br/edu/ifsul/sapucaia/projeto/security/controller/request/EnviarEmailRequest.java
@@ -3,15 +3,17 @@ package br.edu.ifsul.sapucaia.projeto.security.controller.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import static br.edu.ifsul.sapucaia.projeto.controller.request.ErroRequest.CAMPO_OBRIGATORIO;
+
 @Builder
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 public class EnviarEmailRequest {
 
-    @NotBlank(message = "Campo obrigatório.")
+    @NotBlank(message = CAMPO_OBRIGATORIO)
     private String assunto;
 
-    @NotBlank(message = "Campo obrigatório.")
+    @NotBlank(message = CAMPO_OBRIGATORIO)
     private String conteudo;
 
 }
